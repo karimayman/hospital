cf9e7476e94159918b6ae5af25cb1b8a4bfc88bd
karimayman <karimayman95@gmail.com>
2018-11-09 15:00:09 +0200

    added the account type attribute

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users , path: '',path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  devise_for :users, controllers: { sessions: 'users/sessions' }, path: '',path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  #devise_for :users 
   resources :forums, except: :index
   root'forums#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
